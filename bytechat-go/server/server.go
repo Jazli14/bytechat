bcff56746cecc97eb9be798b7e18658c5e9c536b
Jazli14 <jazlim140@gmail.com>
2026-03-10 21:48:55 -0400

    Hub pattern used

--- bytechat-go/server/server.go
+++ bytechat-go/server/server.go
@@ -8,7 +8,48 @@ import (
 	"os"
 )
 
-func handleConnection(conn net.Conn, id int, clients *[]net.Conn) {
+type Hub struct {
+	clients    map[net.Conn]struct{}
+	register   chan net.Conn
+	unregister chan net.Conn
+	broadcast  chan Message
+}
+
+type Message struct {
+	sender  net.Conn
+	content []byte
+}
+
+func newHub() *Hub {
+	return &Hub{
+		clients:    make(map[net.Conn]struct{}),
+		register:   make(chan net.Conn),
+		unregister: make(chan net.Conn),
+		broadcast:  make(chan Message),
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select {
+		case conn := <-h.register:
+			h.clients[conn] = struct{}{}
+		case conn := <-h.unregister:
+			delete(h.clients, conn)
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				if client != message.sender {
+					_, err := client.Write(message.content)
+					if err != nil {
+						panic("Could not write to client")
+					}
+				}
+			}
+		}
+	}
+}
+
+func handleConnection(conn net.Conn, id int, hub *Hub) {
 	defer func() {
 		if err := conn.Close(); err != nil {
 			panic("Connection could not be closed")
@@ -20,6 +61,7 @@ func handleConnection(conn net.Conn, id int, clients *[]net.Conn) {
 		n, err := conn.Read(buffer)
 		if err != nil {
 			if err == io.EOF {
+				hub.unregister <- conn
 				fmt.Println("Client disconnected")
 			} else {
 				fmt.Println("Error reading from connection: ", err)
@@ -28,36 +70,20 @@ func handleConnection(conn net.Conn, id int, clients *[]net.Conn) {
 		}
 		readBuffer := string(buffer[:n])
 		message := fmt.Sprintf("Client %d: %s", id, readBuffer)
-		for _, client := range *clients {
-			if client != conn {
-				_, err := client.Write([]byte(message))
-				if err != nil {
-					fmt.Println("Client disconnected, can't send")
-					return
-				}
-
-			}
-		}
+		hub.broadcast <- Message{sender: conn, content: []byte(message)}
 		fmt.Println(message)
 	}
 }
 
-func broadcastMessage(clients *[]net.Conn) {
+func broadcastMessage(hub *Hub) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		for _, c := range *clients {
-			_, err := c.Write([]byte(scanner.Text()))
-			if err != nil {
-				fmt.Println("Client disconnected, can't send")
-				return
-			}
-
-		}
+		hub.broadcast <- Message{sender: nil, content: []byte("Server: " + scanner.Text())}
 	}
 }
 
 func main() {
-	var clients []net.Conn
+	hub := newHub()
 	idCount := 0
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -72,7 +98,8 @@ func main() {
 
 	fmt.Println("Server listening on :8080")
 
-	go broadcastMessage(&clients)
+	go broadcastMessage(hub)
+	go hub.run()
 
 	for {
 		conn, err := listener.Accept()
@@ -80,10 +107,9 @@ func main() {
 			fmt.Println("Could not accept connection: ", err)
 			continue
 		}
-		clients = append(clients, conn)
+		hub.register <- conn
 
 		idCount++
-		go handleConnection(conn, idCount, &clients)
-
+		go handleConnection(conn, idCount, hub)
 	}
 }
